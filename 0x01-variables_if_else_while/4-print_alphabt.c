1d89dc28f7448e0143827299d53ee2666ceaa899
kuku2k22 <christokombi03@gmail.com>
2022-12-09 17:07:32 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+/**
+ * main - main block
+ * Description: get a randomn number and check its
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z');
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+
+		}
+		c++
+	}
+	putchar('\n');
+	return (0);
+}
