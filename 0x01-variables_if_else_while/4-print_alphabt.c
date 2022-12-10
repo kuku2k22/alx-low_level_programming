854fffe747779dd45449042f204b95a71064644f
kuku2k22 <christokombi03@gmail.com>
2022-12-09 17:09:49 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,14 @@ int main(void)
 {
 	char c = 'a';
 
-	while (c <= 'z');
+	while (c <= 'z')
 	{
 		if (c != 'e' && c != 'q')
 		{
 			putchar(c);
 
 		}
-		c++
+		c++;
 	}
 	putchar('\n');
 	return (0);
