d80c4f19e37753f2e3fdd7c37948ade7b8c33161
kuku2k22 <christokombi03@gmail.com>
2022-12-13 17:58:49 -0800

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int a, b;
+	int a;
+	int b;
 
 	for (a = 0; a <= 23; a++)
 	{
 		for (b = 0; b <= 59; b++)
 		{
-			_putchar((a / 10) + '0');
-			_putchar((a % 100) + '0');
+			_putchar(a / 10 + '0');
+			_putchar(a % 100 + '0');
 			_putchar(':');
-			_putchar((b / 10) + '0');
-			_putchar((b % 10) + '0');
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
 			_putchar('\n');
 		}
 	}
